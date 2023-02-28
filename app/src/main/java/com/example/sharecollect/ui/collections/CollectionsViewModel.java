c584d622932e178e5283b9eefe2cf73e09ee19c3
HugoC <hugo.coutant@yahoo.com>
2023-02-28 22:53:41 +0100

    Renommage et creation fragments de base

--- app/src/main/java/com/example/sharecollect/ui/collections/CollectionsViewModel.java
+++ app/src/main/java/com/example/sharecollect/ui/collections/CollectionsViewModel.java
@@ -1,16 +1,16 @@
-package com.example.sharecollect.ui.notifications;
+package com.example.sharecollect.ui.collections;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class CollectionsViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public CollectionsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is collections fragment");
     }
 
     public LiveData<String> getText() {
