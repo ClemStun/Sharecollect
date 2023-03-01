ddd950f0f9f0471e8cb0f14fc6456631bba18b36
ClemStun <stundeveloppe@gmail.com>
2023-03-01 23:36:49 +0100

    Lien bouton activité
    Lien entre le bouton de création et l'activité de création de collection.

---
+++ app/src/main/java/com/example/sharecollect/ui/collections/CollectionCreateActivity.java
@@ -0,0 +1,16 @@
+package com.example.sharecollect.ui.collections;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sharecollect.R;
+
+public class CollectionCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collection_create);
+    }
+}
--- app/src/main/java/com/example/sharecollect/ui/collections/CollectionsFragment.java
+++ app/src/main/java/com/example/sharecollect/ui/collections/CollectionsFragment.java
@@ -1,6 +1,8 @@
 package com.example.sharecollect.ui.collections;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.sharecollect.databinding.FragmentCollectionsBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CollectionsFragment extends Fragment {
 
@@ -24,12 +27,27 @@ public class CollectionsFragment extends Fragment {
         binding = FragmentCollectionsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        FloatingActionButton create_button = binding.createButton;
+        create_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createCollection();
+            }
+        });
+
         return root;
     }
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
+    private void createCollection() {
+
+        Intent intent = new Intent(getActivity(), CollectionCreateActivity.class);
+        startActivity(intent);
+
+    }
+
 }
