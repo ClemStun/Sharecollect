e42f2bbd37f9576cb7812019a49b663c9a4d69b0
ClemStun <stundeveloppe@gmail.com>
2023-04-03 21:20:16 +0200

    Add image to profile and can take image for item

--- app/src/main/java/com/example/sharecollect/ItemAdapter.java
+++ app/src/main/java/com/example/sharecollect/ItemAdapter.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.sharecollect.models.Item;
+
 import java.util.List;
 
 /**
